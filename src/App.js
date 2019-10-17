d5731fdf096db4877dbdd4618968106ff8b20147
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2019-10-17 00:15:32 -0400

    upload local files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from './Components/Nav/Nav';
+import LeftNav from './Components/LeftNav/LeftNav';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Nav />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React and TypeScript content
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <p>
+          Data from API call lands here
+        </p>
+      <LeftNav />
     </div>
   );
 }
---
+++ src/Components/LeftNav/LeftNav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LeftNav.css';
+
+function LeftNav() {
+  return (
+    <div className="LeftNav">
+          Left nav component
+    </div>
+  );
+}
+
+export default LeftNav;
